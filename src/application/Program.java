e0905e98f24640d17dff36865271198ef0f14ef4
Henrique Lira <henrique.melhado@hotmail.com>
2020-06-16 21:57:32 -0300

    Projeto terminado

---
+++ src/application/Program.java
@@ -0,0 +1,78 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		//...DADOS DO CLIENTE
+		System.out.println("-Enter client data:");
+		System.out.print("Name: ");
+		String clientName = sc.nextLine();
+		System.out.print("Email: ");
+		String clientEmail = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date clientBirthDate = sdf.parse(sc.next());
+
+		Client client = new Client(clientName, clientEmail, clientBirthDate);
+		
+		//...DADOS DO PEDIDO
+		System.out.println("-Enter order data:");
+		System.out.print("Status: ");
+		OrderStatus status = OrderStatus.valueOf(sc.next());
+		
+		Order order = new Order(new Date(), status, client);
+		
+		//...OS PEDIDOS
+		System.out.print("How many items to this order? ");
+		int n = sc.nextInt();	
+		for (int i = 1; i <= n; i++) {
+			System.out.println();
+			System.out.println("Enter #" + i + " data:");
+			sc.nextLine();
+			System.out.print("Product name: ");
+			String productName = sc.nextLine();
+			System.out.print("Product price: ");
+			Double productPrice = sc.nextDouble();
+			
+			Product product = new Product(productName, productPrice);
+			
+			System.out.print("Quantity: ");
+			int quantity = sc.nextInt();
+			
+			OrderItem orderItem = new OrderItem(quantity, productPrice, product);
+			
+			order.addItem(orderItem);
+		}
+		
+		System.out.println();
+		System.out.println("ORDER SUMARY:");
+		System.out.println(order);
+
+		sc.close();
+	}
+
+}
+
+//...INSTANCIA합ES E ASSOCIA합ES. (outra forma enxuta)
+
+//Order order = new Order(new Date(), OrderStatus.valueOf(status), new Client(clientName, clientEmail, clientBirthDate));
+
+//...INSTANCIA플O DOS OBJETOS :OrdemItem e :Product; ASSOCIA플O COM O OBJETO :Order. (outra forma enxuta)
+
+//OrderItem oderItem = new OrderItem(quantity, productPrice, new Product(productName, productPrice));
+//order.addItem(oderItem);
